810e1a9a5cefe36ba9ff5dbac274d7963963ded1
Chris Funderburg <chris@funderburg.me>
2016-12-12 10:31:55 +0000

    changed to run on all nodes at once

--- check-repair.sh
+++ check-repair.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
-then
-  echo -e "\nUsage:\n$0 <hostname> <username> <password>"
-  exit 1
-fi
-
-TARGET_HOST=$1
-USER=$2
-PASS=$3
-
-for hostname in $( nodetool -h $TARGET_HOST -u $USER -pw $PASS status | sed -e '1,5d' | awk '/[U]N/ {print $2}' )
+for target in $( nodetool -h localhost status | sed -e '1,5d' | awk '/[U]N/ {print $2}' )
 do
-  echo Checking $hostname for ongoing repairs
-  nodetool -h $hostname -u $USER -pw $PASS tpstats | grep Repair#
-  response=$?
-  if [ $response -eq 0 ]
-  then
-    repair_ongoing=true
-    echo "Ongoing repair on $hostname"
-  fi
-done
 
-if ! [ $repair_ongoing ]
-then
-  echo "Taking a snapshot."
-  nodetool -h $TARGET_HOST -u $USER -pw $PASS snapshot
-  echo "Starting repair on $TARGET_HOST"
-  nodetool -h $TARGET_HOST -u $USER -pw $PASS repair
-fi
+    echo "=== Target $target ==="
+
+    for hostname in $( nodetool -h $target status | sed -e '1,5d' | awk '/[U]N/ {print $2}' )
+    do
+      echo Checking $hostname for ongoing repairs | sed 's/^/    /g'
+      nodetool -h $hostname tpstats | grep -q Repair#
+      response=$?
+      if [ $response -eq 0 ]
+      then
+        repair_ongoing=true
+        echo "Ongoing repair on $hostname" | sed 's/^/    /g'
+      fi
+    done
+
+    echo
+
+    if ! [ $repair_ongoing ]
+    then
+      echo "Taking a snapshot." | sed 's/^/    /g'
+      nodetool -h $target snapshot | sed 's/^/        /g'
+      echo "Starting repair on $TARGET_HOST" | sed 's/^/    /g'
+      nodetool -h $target repair -pr | sed 's/^/        /g'
+    fi
+    echo
+
+done
